f9218f062eb245418487bc1dcab1fbbde46b67eb
Trevor Rogers <trevorrogers@Trevors-MacBook-Pro.local>
2024-09-20 11:54:21 +0100

    added a sort button, worked  on some error handling and feedback

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -5,41 +5,84 @@ import { Link, useSearchParams } from "react-router-dom";
 const Articles = ({articles, setArticles}) => {
 
     const [searchParams, setSearchParams] = useSearchParams();
-    const topicsQuery = searchParams.get("topic")
-    const [currentTopic, setCurrentTopic] = useState("")
+    const topicsQuery = searchParams.get("topic");
+    const sortByQuery = searchParams.get("sort_by");
+    const [currentTopic, setCurrentTopic] = useState("");
+    const [currentSortBy, setCurrentSortBy] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false)
 
 
     useEffect(() => {
-        getArticles(topicsQuery)
+        setIsLoading(true)
+        getArticles(topicsQuery, sortByQuery)
           .then(({articles}) => {
           setArticles(articles)
           setCurrentTopic(topicsQuery)
+          setCurrentSortBy(sortByQuery)
+          setIsLoading(false)
+          setIsError(false)
           })
           .catch((err) => {
+            setIsError(true)
+            setIsLoading(false)
             console.log('in catch', err)
           });
-      }, [topicsQuery]);
+      }, [topicsQuery, sortByQuery]);
 
       function topicSort(topicChoosen) {
+        if(topicsQuery === "All") {
+            console.log("here")
+        } else {
+            const newParams = new URLSearchParams(searchParams);
+            newParams.set("topic", topicChoosen);
+            setSearchParams(newParams);
+            setCurrentTopic(topicChoosen);
+        }
+      }
+      function sortBySort(sortBy) {
         const newParams = new URLSearchParams(searchParams);
-        newParams.set("topic", topicChoosen);
+        newParams.set("sort_by", sortBy);
         setSearchParams(newParams);
-        setCurrentTopic(topicChoosen);
+        setCurrentSortBy(sortBy);
+      }
+
+      if (isError) {
+        return (
+            <p>Not Found</p>
+        )
+      }
+
+      if (isLoading) {
+        return (
+         <p>Loading</p>
+        );
       }
 
   return (
     <main className="articles">
+        <p>Topics</p>
         <select name="" id="" onChange={(event)=> {
             topicSort(event.target.value)
         }}>Topics 
         <option>All</option>
-        {articles.map((article, i) => {
-          return (
-            <option key={i} value={article.topic}>
-                {article.topic}
-            </option>
-          );
-        })}
+        <option>football</option>
+        <option>coding</option>
+        <option>cooking</option>
+       
+        </select>
+        <p>Sort articles
+        </p>
+        <select name="" id="" onChange={(event)=> {
+            sortBySort(event.target.value)
+        }}>Topics 
+        <option>All</option>
+        <option>title</option>
+        <option>topic</option>
+        <option>author</option>
+        <option>created_at</option>
+        <option>votes</option>
+        <option>comment_count</option>
         </select>
         {articles.map((article, article_id) => {
             return (
